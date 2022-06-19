f85db9f4bcb193ed1b6a40d39a2d029f3c0ad906
Alexander <Alexander_alatorre1993@hotmail.com>
2022-06-18 18:45:54 -0600

    first commit

---
+++ src/shared/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import routes from "./routes";
+import { Route, Link, Redirect, Switch } from "react-router-dom";
+import Navbar from "./Navbar";
+import NoMatch from "./NoMatch";
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Switch>
+      {routes.map(({ path, exact, component: Component, ...rest }) => (
+        <Route
+          key={path}
+          path={path}
+          exact={exact}
+          render={(props) => <Component {...props} {...rest} />}
+        />
+      ))}
+      <Route render={(props) => <NoMatch {...props} />} />
+    </Switch>
+  </div>
+);
+
+export default App;
---
+++ src/shared/NoMatch.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const NoMatch = () => <div>Four Oh Four</div>;
